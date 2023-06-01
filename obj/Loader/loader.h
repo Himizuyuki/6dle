fe6986c370730560e914b5769f4714032a8344a3
Arthur Gailleul <arthur.gailleul@epita.fr>
2023-06-01 19:34:43 +0200

    tree: + remove | fix

---
+++ obj/Loader/loader.h
@@ -0,0 +1,12 @@
+#ifndef LOADER_H
+#define LOADER_H
+
+// I need a function that:
+//  -loads the Word Bank txt
+//  -adds a word to the Word bank (with or without the tree)
+//  -Sort the Word Bank in alphabetical order
+//  -remove a word from the word bank (with or without the tree)
+
+#include "../treeGen/tree.h"
+
+#endif // DEBUG
--- obj/treeGen/tree.c
+++ obj/treeGen/tree.c
@@ -33,7 +33,7 @@ int addWord(char* word, Tree* tree){
 			if (tmp->depth < 6) {
 			
 				tmp->child = malloc(26 * sizeof(Tree));
-				for (size_t j = 0; j < 25; j++){
+				for (size_t j = 0; j <= 25; j++){
 					
 					tmp->child[j]->letter = 0;
 					tmp->child[j]->depth = tmp->depth + 1;
@@ -47,12 +47,57 @@ int addWord(char* word, Tree* tree){
 	return 1;
 }
 
+int removeWordAux(char* word, Tree* tree, int i){
+	
+	if (tree->depth == 6)
+
+		if (tree->letter != 1)
+			return 0;
+		else {
+			tree->letter = 0;
+			return 1;
+		}
+	else{
+		
+		if (tree->child[(word[i] - 19) % 26]->letter == 1){
+			
+			int res = removeWordAux(word, tree->child[(word[i] - 19) % 26], i+1);
+			if (res == 0)
+				return 0;
+			else{
+
+				tree->letter = 0;
+				// go through the children list, if there aren't any letters , free and set to null
+				size_t j = 0;
+				for (; j <= 25;){
+					
+					if (tree->child[(word[i] - 19) % 26]->letter == 1)
+						break;
+					j++;
+				}
+				if (j == 26){
+					free(tree->child);
+				}
+				return 1;
+			}
+		}
+		else {
+			return 0;
+		}
+	}
+}
+
+// assuming the word is a 6 letter word
+int removeWord(char* word, Tree* tree){
+	return removeWordAux(word, tree, 0);
+}
+
 void freeTreeAux(Tree* tree){
 
 	if (tree == NULL)
 		return;
 	else{
-		for (size_t i = 0; i < 25; i++){
+		for (size_t i = 0; i <= 25; i++){
 
 			freeTreeAux(tree->child[i]);
 		}
