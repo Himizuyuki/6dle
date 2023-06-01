a994e43ee6a05611510c32f71fa76b0946c15142
Arthur Gailleul <arthur.gailleul@epita.fr>
2023-06-01 19:59:50 +0200

    loading

---
+++ obj/Loader/loader.c
@@ -0,0 +1,8 @@
+#include "loader.h"
+
+// I need a function that:
+//  -loads the Word Bank txt (from a path)
+//      .check for the length of the words (see if its possible to remove it after)
+//  -adds a word to the Word bank (with or without the tree)
+//  -Sort the Word Bank in alphabetical order
+//  -remove a word from the word bank (with or without the tree)
