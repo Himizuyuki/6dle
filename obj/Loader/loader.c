dfc2a6f6d44973b68a969eff90784ce62fb5533e
Arthur Gailleul <arthur.gailleul@epita.fr>
2023-06-08 01:24:30 +0200

    fixed loader

--- obj/Loader/loader.c
+++ obj/Loader/loader.c
@@ -4,6 +4,7 @@
 #include <aio.h>
 #include <err.h>
 #include <ctype.h>
+#include <string.h>
 #include "../treeGen/tree.h"
 // I need a function that:
 //  -loads the Word Bank txt (from a path)
@@ -11,36 +12,27 @@
 //  -adds a word to the Word bank (with or without the tree)
 //  -Sort the Word Bank in alphabetical order
 //  -remove a word from the word bank (with or without the tree)
-Tree* Tloader(const char* path){
+Tree* Tloader(const char* path) {
+    // Call the shell script to remove invalid words
+    char command[100];
+    sprintf(command, "./remove_invalid_words.sh %s", path);
+    system(command);
 
     Tree* tree = initTree();
-    FILE* f;
-    char* line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    f = fopen(path, "r");
-    if (f == NULL)
-        errx(EXIT_FAILURE, "EXIT_FAILURE");
-    char word[7];
-    while (fscanf(f, "%6s", word) == 1) {
-        printf("Word :%s | length : %zu\n", line, len);
-        if (len != 6) // 6 letter word + \n
-            //TODO: Remove Word from txt file
-            errx(EXIT_FAILURE, "Not implemented: Word too long");
-        else{
-            for (size_t i = 0; i < 6; i++){
-                word[i] = tolower(line[i]);
-            }
-            word[6] = 0;
-            printf("%s\n", word);
-            addWord(word, tree);
+    FILE* f = fopen(path, "r+");
+    if (f == NULL) {
+        errx(EXIT_FAILURE, "Failed to open file");
+    }
+    char word[7]; // Adjusted to accommodate 12-letter words
+    //long int filePos = ftell(f);
+    while (fscanf(f, "%6s", word) == 1) { // Updated to read up to 12 characters
+        size_t len = strlen(word);
+        for (size_t i = 0; i < len; i++) {
+            word[i] = tolower(word[i]);
         }
+        addWord(word, tree);
     }
     fclose(f);
-    if (line)
-        free(line);
     return tree;
 }
-
 
